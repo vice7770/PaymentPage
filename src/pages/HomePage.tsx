073996b9711c535b977b808c15139afa4feaf566
vicente almeida <vicente.cra45@gmail.com>
2024-08-09 19:59:53 +0400

    Basic templating

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useExpenses,useActualPage, useNumberOfItems, useNumberOfPages, useWeatherActions } from '../stores/expenses';
+import { useExpenses,useActualPage, useNumberOfItems, useNumberOfPages, useWeatherActions } from '../stores/expenses';\
+
+const Card = (expense : any) => {
+  return (
+    <div>
+      <h2>Expense</h2>
+      <p>Amount: </p>
+      <p>Category: </p>
+      <p>Date: </p>
+    </div>
+  );
+}
+
 const HomePage = () => {
   const expenses = useExpenses();
   /// the idea for this component would be to fetch in zusntad initializer the expenses and then use the useExpenses hook to get the expenses
@@ -11,7 +23,11 @@ const HomePage = () => {
   return (
     <div>
       <h1>Expenses</h1>
-      
+      <ul>
+        {expenses.map((expense) => (
+          <Card expense={expense}/>
+        ))}
+      </ul>
     </div>
   );
 };
